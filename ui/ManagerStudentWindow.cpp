4d7c0462f08607b2b87d77e29aed650bbe25a94e
Zhiming <xiaoxiaomingbao@gmail.com>
2025-03-29 18:39:36 +0800

    manager student window: set table column name, do not display line number

--- ui/ManagerStudentWindow.cpp
+++ ui/ManagerStudentWindow.cpp
@@ -6,6 +6,7 @@
 #include <QVBoxLayout>
 #include <QMessageBox>
 #include <QComboBox>
+#include <QHeaderView>
 #include <QDebug>
 
 ManagerStudentWindow::ManagerStudentWindow(QWidget *parent) : QWidget(parent) {
@@ -51,9 +52,19 @@ void ManagerStudentWindow::setupUI() {
 void ManagerStudentWindow::setupDatabase() {
     model = new QSqlTableModel(this);
     model->setTable("students");
+
+    // 设置表格的列名
+    model->setHeaderData(0, Qt::Horizontal, "学号");
+    model->setHeaderData(1, Qt::Horizontal, "姓名");
+    model->setHeaderData(2, Qt::Horizontal, "性别");
+    model->setHeaderData(3, Qt::Horizontal, "班级");
+
     model->select();
 
     tableView->setModel(model);
+
+    // 不显示行号
+    tableView->verticalHeader()->setVisible(false);
 }
 
 void ManagerStudentWindow::addStudent() {
