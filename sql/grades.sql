4a211f71e5d208152ff57d533513d0ad8ba9ff79
Zhiming <xiaoxiaomingbao@gmail.com>
2025-03-31 23:08:22 +0800

    score in grades can be null, representing course selection

--- sql/grades.sql
+++ sql/grades.sql
@@ -2,7 +2,7 @@ CREATE TABLE grades (
     grade_id    INTEGER PRIMARY KEY AUTOINCREMENT,   -- 成绩 ID
     student_id  INTEGER NOT NULL,                    -- 学生 ID
     course_id   INTEGER NOT NULL,                    -- 课程 ID
-    score       DECIMAL(4,1) NOT NULL,               -- 分数
+    score       DECIMAL(4,1),                        -- 分数
     FOREIGN KEY (student_id) REFERENCES students(student_id),
     FOREIGN KEY (course_id) REFERENCES courses(course_id)
 );
